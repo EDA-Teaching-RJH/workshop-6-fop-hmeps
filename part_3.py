3da75bc62e0377d0cec5a5e289a2b8efefdcc46d
hmeps <hm18@kent.ac.uk>
2026-02-10 13:47:33 +0000

    part 3.3

--- part_3.py
+++ part_3.py
@@ -5,8 +5,12 @@ def get_coordinate():
         while True:
             try:
                 _ = int(input("Enter an X-coordinate integer: "))
-                print(_)
-                break
+                if 100 < _  < -100:
+                    print("Coordinate out of Range!")
             except ValueError:
                 print("Invalid Coordinate")
+            else:
+               break
 main()
+
+#unsure how to include the if statement into the loop***
