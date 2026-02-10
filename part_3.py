2e95bda03444b6c74beee32760ad228911347647
hmeps <hm18@kent.ac.uk>
2026-02-10 13:38:19 +0000

    part 3.2

--- part_3.py
+++ part_3.py
@@ -1,5 +1,12 @@
-try:
-    motor_speed = int(input("Enter Motor Speed: "))
-    print(f"Speed set to {motor_speed}mph!")
-except ValueError:
-    print("Error: Corrupted Signal. Maintaining Current Speed")
+def main():
+    get_coordinate()
+
+def get_coordinate():
+        while True:
+            try:
+                _ = int(input("Enter an X-coordinate integer: "))
+                print(_)
+                break
+            except ValueError:
+                print("Invalid Coordinate")
+main()
