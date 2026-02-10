ea8aeed49873c37cfa114bd2227c81b8e8c8f592
hmeps <hm18@kent.ac.uk>
2026-02-10 13:01:03 +0000

    part 2.2

--- part_2.py
+++ part_2.py
@@ -1,5 +1,6 @@
-rover_status = {'Battery': 100,
-                'Heater': 'Off',
+rover_status = {'Battery': 85,
+                'Heater': 'On',
                 'Camera': 'Standby',
+                'Speed' : 5,
                 }
-print(rover_status['Battery'])
+print(rover_status)
