56479e387ef0bc0006bf046b604a350d0a54d12f
hmeps <hm18@kent.ac.uk>
2026-02-10 12:36:46 +0000

    part 1.3

--- part_1.py
+++ part_1.py
@@ -1,3 +1,5 @@
-sample_bay = ['Basalt', 'Silica', 'Iron', 'Dust']
-for samples in range(len(sample_bay)):
-    print("Transmitting Data For:",sample_bay[samples])
+new_findings = []
+for _ in range(3):
+    rock_type = str(input("What type of rock did you find? "))
+    new_findings.append(rock_type)
+print(new_findings)
