a4afbb58fa3e4af4494e661283b8381dd50e40b1
hmeps <hm18@kent.ac.uk>
2026-02-10 12:44:07 +0000

    part 1.4

--- part_1.py
+++ part_1.py
@@ -1,5 +1,4 @@
-new_findings = []
-for _ in range(3):
-    rock_type = str(input("What type of rock did you find? "))
-    new_findings.append(rock_type)
-print(new_findings)
+sample_bay = ['Granite', 'Silica', 'Iron', 'Dust']
+if 'Dust' in sample_bay:
+    sample_bay.remove('Dust')
+print(sample_bay)
